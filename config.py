81e3248ebb414b37bd847c560b677a0a6f29502a
Devbot33 <debojitbk08@gmail.com>
2025-02-11 17:40:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ ADS_MODE = getenv("ADS_MODE", None)
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", None))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
